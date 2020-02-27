53110223b1e320f6b4861cbd6622faa0b0a24fdc
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2020-02-27 18:05:32 +0200

    Create toolbar learning activity

--- app/src/main/java/com/learn/everything/MainActivity.kt
+++ app/src/main/java/com/learn/everything/MainActivity.kt
@@ -2,10 +2,13 @@ package com.learn.everything
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.learn.everything.toolbar.LearnToolbarActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startActivity(LearnToolbarActivity.createIntent(this))
+        finish()
     }
 }
---
+++ app/src/main/java/com/learn/everything/toolbar/LearnToolbarActivity.kt
@@ -0,0 +1,24 @@
+package com.learn.everything.toolbar
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.learn.everything.R
+import kotlinx.android.synthetic.main.activity_learn_toolbar.*
+
+class LearnToolbarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_learn_toolbar)
+        setSupportActionBar(toolbar)
+        toolbar.setOnClickListener {
+            supportActionBar?.hide()
+        }
+    }
+
+    companion object {
+        fun createIntent(context: Context) =
+            Intent(context, LearnToolbarActivity::class.java)
+    }
+}
