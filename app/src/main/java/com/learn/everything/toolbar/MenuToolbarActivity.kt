18ea74d335f567bf9d8448026395d172caadbcdf
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2020-02-27 19:49:56 +0200

    Add click in onOptionsItemSelected in MenuToolbarActivity.kt

--- app/src/main/java/com/learn/everything/toolbar/MenuToolbarActivity.kt
+++ app/src/main/java/com/learn/everything/toolbar/MenuToolbarActivity.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.learn.everything.R
 import kotlinx.android.synthetic.main.activity_learn_toolbar_simple.*
@@ -18,6 +20,14 @@ class MenuToolbarActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_toolbar, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.openSettingButton -> {
+            Toast.makeText(this, "click", Toast.LENGTH_SHORT).show()
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 
     companion object {
