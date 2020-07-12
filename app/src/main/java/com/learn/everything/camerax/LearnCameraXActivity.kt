eb98464b16e7ef385627fcf6e1f0a6a7a4eacf26
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2020-07-12 21:02:26 +0300

    Add working official CameraX sample

--- app/src/main/java/com/learn/everything/camerax/LearnCameraXActivity.kt
+++ app/src/main/java/com/learn/everything/camerax/LearnCameraXActivity.kt
@@ -5,14 +5,15 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.learn.everything.R
+import com.learn.everything.camerax._01_official_sample.OfficialSample
 import kotlinx.android.synthetic.main.activity_learn_camera_x.*
 
 class LearnCameraXActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_learn_camera_x)
-        basicUsageButton.setOnClickListener {
-            startActivity(BasicUsageActivity.createIntent(this))
+        officialSampleButton.setOnClickListener {
+            startActivity(OfficialSample.createIntent(this))
         }
     }
 
